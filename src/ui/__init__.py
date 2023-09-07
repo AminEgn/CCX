6448074b529bd7c72a089465abdbebe38a2dac76
amin-egn <aminx15am@yahoo.com>
2023-09-07 14:09:21 +0330

    #__init__.py ~ src/ui/ -> 1) add _createMenu, _createLanding methods

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,5 +1,6 @@
 # internal
 from . import widgets
+from .components import ActionButton
 
 # pyqt
 from PyQt5.QtCore import Qt
@@ -20,6 +21,7 @@ class UI(QtWidgets.QMainWindow):
         self._setStyle()
 
     def _createUI(self):
+        self.setMinimumSize(900, 460)
         # central widget
         self._centralWidget = QtWidgets.QWidget(self)
         self.setCentralWidget(self._centralWidget)
@@ -29,9 +31,43 @@ class UI(QtWidgets.QMainWindow):
         self.generalLayout.setSpacing(0)
         self.generalLayout.setAlignment(Qt.AlignTop)
         self._centralWidget.setLayout(self.generalLayout)
+        # instance
+        self._createFrames()
+
+    def _createMenu(self):
+        self._menuWidget = QtWidgets.QWidget(self)
+        self._menuLayout = QtWidgets.QVBoxLayout()
+        self._menuWidget.setLayout(self._menuLayout)
+        # buttons
+        self.btnCustomer = ActionButton("Customer")
+        self.btnGroup = ActionButton("Group")
+        self.btnSettings = ActionButton("Settings")
+        self._menuButtons = (
+            self.btnCustomer,
+            self.btnGroup,
+            self.btnSettings,
+        )
+        for btn in self._menuButtons:
+            self._menuLayout.addWidget(btn)
+
+        # attach
+        self._landingLayout.addWidget(self._menuWidget)
+
+    def _createLanding(self):
+        self._landingWidget = QtWidgets.QWidget(self)
+        self._landingLayout = QtWidgets.QHBoxLayout()
+        self._landingWidget.setLayout(self._landingLayout)
+        # instance
+        self.dashboardFrame = widgets.DashboardFrame(self)
+        self._createMenu()
+        # attach
+        # - landing
+        self._landingLayout.addWidget(self.dashboardFrame)
+        # - general
+        self.generalLayout.addWidget(self._landingWidget)
 
     def _createFrames(self):
-        pass
+        self._createLanding()
 
     def _connectedSignals(self):
         pass
